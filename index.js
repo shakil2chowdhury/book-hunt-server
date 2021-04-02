f5d32f234fd063164189c1e1e8f1bc45b52565dd
unknown <shakil2chowdhury@gmail.com>
2021-04-02 23:51:58 +0600

    port fix

--- index.js
+++ index.js
@@ -49,6 +49,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port || process.env.PORT)
